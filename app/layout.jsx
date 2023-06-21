6641191d30d30923b52dc7b18f3cc995ed28aad4
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-06-21 14:58:34 +0530

    initial setup

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import "@styles/globals.css";
+
+
+export const metadata = {
+  title: "MyAnimeList",
+  description: "Never forget your favorite anime again!",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang='en'>
+    <body>
+      {/* <Provider> */}
+        <div className='main'>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          {children}
+        </main>
+      {/* </Provider> */}
+    </body>
+  </html>
+);
+
+export default RootLayout;
